a017da6b5c3b7f30bba9e76badc763d4b7766ad7
JE <wjd7dms7@naver.com>
2023-07-04 05:29:27 +0900

    #3.4 State

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 
 // interface : 객체의 타입을 정의
@@ -25,6 +25,9 @@ interface CircleProps {  // function의 타입을 정의
 }
                                                     // text의 디폴트 값 정의
 export default function Circle({bgColor, borderColor, text = "default text"}: CircleProps) {
+    // const [value, setValue] = useState<number|string>(0); // 디폴트 값으로 Typescript가 충분히 유추하지만 맞춤제작을 하고싶을때 타입을 선언해준다
+    // setValue(2)
+    // setValue("hello")
   return (
 
                                             // borderColor값이 있다면 그 값을 이용하고, 값이 없다면 bgColor가 디폴트 값이다
