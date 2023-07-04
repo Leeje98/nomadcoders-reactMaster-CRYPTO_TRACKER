f22a19caef70902a163a9794bc31c3916663ddf5
JE <wjd7dms7@naver.com>
2023-07-04 15:23:40 +0900

    #3.7 Recap

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,42 @@ import styled from "styled-components";
 import Circle from "./Circle";
 import { useState } from "react";
 
-function App() {
-  
-  const Container = styled.div`
-    background-color: ${(props) => props.theme.bgColor}; 
-  `;
-  const H1 = styled.h1`
-    color: ${(props) => props.theme.textColor};
-  `;
 
+const Container = styled.div`
+background-color: ${(props) => props.theme.bgColor}; 
+`;
+const H1 = styled.h1`
+color: ${(props) => props.theme.textColor};
+`;
+interface DummyProps {
+  text: string;
+  active?: boolean;
+}
+
+function Dummy ({text, active = false}: DummyProps) {
+  return (
+    <H1>{text}</H1>
+  )
+}
+
+// function App() { 
+//   const onClick = (event:React.FormEvent<HTMLButtonElement>) => {}
+//   return (
+//     <Container>
+//        <Dummy active text='hello' />
+//        {/* <Dummy active={true} text='hello' /> - 위의 내용과 같다*/}
+//        <form>
+//         <button onClick={onClick}>click me</button>
+//        </form>
+//     </Container>
+//   );
+// }
+function App() {        // react-event이름-event를 발생시키는 Element이름을 사용하는 태그(객체) 
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {}
   return (
     <Container>
-       <H1>protected</H1>
+      <Dummy active text='hello' />
+      <button onClick={onClick}>click me</button>
     </Container>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
