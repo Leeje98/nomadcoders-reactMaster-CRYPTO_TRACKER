1f9ca18738e41f3dca93ec5284c12d07f385fb5b
JE <wjd7dms7@naver.com>
2023-07-04 14:52:42 +0900

    #3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,18 @@ import Circle from "./Circle";
 import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = e;
-    setValue(value)
-  };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
-  };
+  
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor}; 
+  `;
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+       <H1>protected</H1>
+    </Container>
   );
 }
 
