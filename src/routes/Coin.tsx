a25a557006aae678d88c37e5caea202c1173471f
JE <wjd7dms7@naver.com>
2023-07-05 23:24:37 +0900

    #5.6 Coin Data

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
-
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -36,7 +35,21 @@ interface RouteState {
 function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  const { state } = useLocation<RouteState>()
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
 
   return (
     <Container>
